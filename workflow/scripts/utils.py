0c2caa92725210078f15cd41c5d7c6814d8eb838
Frenchants <frederic_busch@yahoo.de>
2024-11-05 14:19:23 +0100

    Added snakemake stuff

--- workflow/scripts/utils.py
+++ workflow/scripts/utils.py
@@ -5,10 +5,3 @@ import numpy as np
 # Adjust the path to point from script.py to src/py_raccoon
 sys.path.append(str((Path(__file__).parent.parent.parent / 'src').resolve()))
 sys.path.append(str((Path(__file__).parent.parent.parent.parent / 'cycleindex').resolve()))
-
-
-# Hier verschiedene balance Parameter berechnen:
-# def calc_ratio
-
-def calc_degree_of_balance(total: np.ndarray, pos: np.ndarray):
-    return pos / total
